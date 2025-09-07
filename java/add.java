a9890cc6d2d05c77431c7181e040b81c1be27c61
Amar Srigade <amarsrigadeoff@gmail.com>
2025-09-07 15:58:34 +0530

    Auto commit - 07-09-2025 15:58:34.67

---
+++ java/add.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class Add {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("hello world and adding two numbers:"+(a+b));
+    }
+}
